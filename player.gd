ed97cc29b69fb514a3c04d85a3bb3422b3470bc3
herouu <herouucn@foxmail.com>
2021-03-28 17:51:05 +0800

    修改

--- player.gd
+++ player.gd
@@ -3,31 +3,37 @@ signal hit
 
 export var speed = 400
 var screen_size
-
-
+var dragging = false
+var click_radius = 32 
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
+	#hide()
 
+func _input(event):
+	
+	if event is InputEventScreenTouch:
+		if (event.position - $AnimatedSprite.position).length() < click_radius:
+		# Start dragging if the click is on the sprite.
+			if not dragging and event.pressed:
+				dragging = true
+		# Stop dragging if the button is released.
+		if dragging and not event.pressed:
+			dragging = false
 
-func _process(delta):
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	if event is InputEventScreenDrag and dragging:
+		# While dragging, move the sprite with the mouse.
+		$AnimatedSprite.position = event.position
+	
+	var velocity = Vector2($AnimatedSprite.position) 
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 
-	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
